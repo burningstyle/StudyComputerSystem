ca741985fc78a4780e46b6c36f7ea58fad693de6
burningstyle <shileide321@gmail.com>
2012-07-20 13:15:13 +0800

    rename expressionBybyte

---
+++ basicLib/show_bytes.h
@@ -0,0 +1,79 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  expressionBybyte.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2012年07月19日 13时56分32秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  BurningStyle
+ *
+ *
+ * =====================================================================================
+ */
+
+#include"stdio.h"
+typedef unsigned char *byte_pointer;
+
+void show_bytes (byte_pointer start,int len);
+void show_int (int x);
+void show_float (float x);
+void show_pointer (void *x);
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  show_bytes
+ *  Description:  
+ * =====================================================================================
+ */
+void
+show_bytes(byte_pointer start,int len)
+{
+    int i=0;
+    for(i=0;i<len;i++){
+        printf ( "%.2x ",start[i]);
+    }
+    printf("\n");  
+}	
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  show_int
+ *  Description:  
+ * =====================================================================================
+ */
+void
+show_int (int x)
+{
+        show_bytes((byte_pointer)&x,sizeof(int));
+}	
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  show_float
+ *  Description:  
+ * =====================================================================================
+ */
+void
+show_float (float x)
+{
+        show_bytes((byte_pointer)&x,sizeof(float));
+}
+
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  show_pointer
+ *  Description:  
+ * =====================================================================================
+ */
+void
+show_pointer (void *x)
+{
+        show_bytes((byte_pointer)&x,sizeof(void *));
+}
