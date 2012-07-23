f17b7ca6ce85a6df40fe2fe80d79aa9b4e6826bf
burningstyle <shileide321@gmail.com>
2012-07-23 12:44:19 +0800

    test

---
+++ test.c
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  06/30/12 20:28:25
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  BurningStyle
+ *
+ *
+ * =====================================================================================
+ */
+
+
+#include	<stdlib.h>
+#include    <stdio.h>
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  main
+ *  Description:  
+ * =====================================================================================
+ */
+int
+main ( int argc, char *argv[] )
+{	
+       int a=0;
+       printf("hello word,burningstyle");
+        return 0;
+        }
+/* ----------  end of function main  ---------- */
